70231e0604ec756f1d960703ad7bb114e0021193
齐谢禹 <2495490010@qq.com>
2016-05-13 16:05:01 +0800

    master 增加数据库操作类及tpl模板文件

---
+++ app/config/database.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * YuYi WorkRoom
+ *
+ * @version 1.1.0
+ * @author  QIXIEYU
+ */
+
+return array(
+    'default' => 'mysql.news',
+    'mysql.news' => array(
+        'driver' => 'mysql',
+        'host' => '127.0.0.1',
+        'database' => 'news',
+        'username' => 'root',
+        'password' => '',
+        'charset' => 'utf8',
+        'collation' => 'utf8_general_ci',
+        'prefix' => 'news_',
+    ),
+);
---
+++ app/models/UserModel.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * YuYi WorkRoom
+ *
+ * @version 1.1.0
+ * @author  QIXIEYU
+ */
+class UserModel extends Model
+{
+    const DATABASE_CONNECTION = 'mysql.news';
+
+    const TABLE_NAME = 'users';
+}
