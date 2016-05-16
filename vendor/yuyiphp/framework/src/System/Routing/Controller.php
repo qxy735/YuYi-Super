25c2a093666e6d538f2828b64dd49b39fd4b77f7
齐谢禹 <2495490010@qq.com>
2016-05-16 16:25:37 +0800

    master 增加模板引擎参数配置

--- vendor/yuyiphp/framework/src/System/Routing/Controller.php
+++ vendor/yuyiphp/framework/src/System/Routing/Controller.php
@@ -8,6 +8,7 @@
  */
 
 use \Smarty;
+use System\Support\Facades\Config;
 
 class Controller extends Smarty
 {
@@ -25,8 +26,28 @@ class Controller extends Smarty
         // 设置模板编译文件目录
         $this->setCompileDir(storage_path() . '/compile');
 
+        // 设置模板引擎配置
+        $this->setTemplateConfig();
+
         // 初始化模板引擎
         parent::__construct();
+    }
+
+    /**
+     * 设置模板引擎配置
+     */
+    protected function setTemplateConfig()
+    {
+        // 获取配置信息
+        $config = Config::get('config');
+
+        // 设置模版使用分隔符号
+        $this->left_delimiter = $config['left_delimiter'];
+        $this->right_delimiter = $config['right_delimiter'];
+
+        // 设置模版缓存信息
+        $this->caching = $config['cache_start'];
+        $this->cache_lifetime = $config['cache_life_time'];
     }
 
     /**
@@ -37,5 +58,31 @@ class Controller extends Smarty
     public function display($path = '')
     {
         parent::display($path);
+    }
+
+    /**
+     * 操作成功跳转提示
+     *
+     * @param string $message
+     * @param string $url
+     * @param int    $time
+     */
+    protected function success($message = '', $url = '', $time = 3)
+    {
+        include Config::get('config.tpl_file_path') . '/' . Config::get('config.success_tpl_file');
+        exit;
+    }
+
+    /**
+     * 操作失败跳转提示
+     *
+     * @param string $message
+     * @param string $url
+     * @param int    $time
+     */
+    protected function error($message = '', $url = '', $time = 3)
+    {
+        include Config::get('config.tpl_file_path') . '/' . Config::get('config.error_tpl_file');
+        exit;
     }
 }
